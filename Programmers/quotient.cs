aad239eb4e3c512c59d56cfa400557a7639acea6
lee hyoseok <lhs19960330@gmail.com>
2024-01-11 09:03:06 +0900

    feat : 몫 구하기 풀이

---
+++ Programmers/quotient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmers
+{
+    internal class quotient
+    {
+        // 몫 구하기
+        int solution(int num1, int num2)
+        {
+            int answer = num1 / num2;
+            return answer;
+        }
+    }
+}
