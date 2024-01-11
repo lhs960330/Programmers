44c464f26c63e2448e2813406d47cf465c72357e
lee hyoseok <lhs19960330@gmail.com>
2024-01-11 09:01:43 +0900

    feat : 나머지 구하기 풀이

---
+++ Programmers/remain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmers
+{
+    internal class remain
+    {
+        //나머지 구하기
+        public class Solution
+        {
+            public int solution(int num1, int num2)
+            {
+                int answer = num1 % num2;
+                return answer;
+            }
+        }
+
+    }
+}
