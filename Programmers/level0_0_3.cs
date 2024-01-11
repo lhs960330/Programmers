8444ceb54d1678942c705414d2fee423046226dd
lee hyoseok <lhs19960330@gmail.com>
2024-01-11 09:04:17 +0900

    feat : 두수의 곱 풀이

---
+++ Programmers/level0_0_3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmers
+{
+    internal class level0_0_3
+    {
+        // 두수의 곱
+        public class Solution
+        {
+            public int solution(int num1, int num2)
+            {
+                int answer = num1 * num2;
+                return answer;
+            }
+        }
+    }
+}
