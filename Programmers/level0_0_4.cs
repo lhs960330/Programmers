bce4fc089999fab7fc44984c3bfd3d6801146995
lee hyoseok <lhs19960330@gmail.com>
2024-01-11 09:06:52 +0900

    feat : 나이 출력 풀이

---
+++ Programmers/level0_0_4.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmers
+{
+    internal class level0_0_4
+    {
+        // 나이 출력
+        public class Solution
+        {
+            public int solution(int age)
+            {
+                int answer = 2023 - age;
+                return answer;
+            }
+        }
+    }
+}
